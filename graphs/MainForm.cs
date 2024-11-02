9c1866a5911b3cf613b9592f3674acced394f125
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-11-01 23:02:17 -0600

    Review and comment code

--- graphs/MainForm.cs
+++ graphs/MainForm.cs
@@ -4,7 +4,8 @@ namespace graphs
 {
     public partial class MainForm : Form
     {
-        public System.Windows.Forms.DataVisualization.Charting.Chart chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+        //Declaring our chart
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
 
         public MainForm()
         {
@@ -14,44 +15,55 @@ namespace graphs
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            chart.Location = new Point(10, 10);
+            //Defines where the chart will be placed
+            chart.Location = new Point(10, 10); 
+            
+            //Using relatives measures to be compatible with all screens
             chart.Size = new Size((int)(this.Width * 0.7), (int)(this.Height * 0.7));
 
+            //ChartArea objects are used to plot the chart.
             chart.ChartAreas.Add(new ChartArea());
             chart.ChartAreas[0].AxisX.LabelStyle.Interval = 1;
 
+            //Defining default Type as Bars
             chart.Series.Add(new Series("Bars"));
             chart.Series[0].ChartType = SeriesChartType.Bar;
             chart.Series[0].IsXValueIndexed = true;
 
+
+            //Adding the graph to the Main Form
             this.Controls.Add(chart);
         }
 
         private void button_Add_Click(object sender, EventArgs e)
         {
+            // Getting typed information
             double value = double.Parse(numericUpDown_Input.Text);
             string type = textBox_Type_Input.Text;
 
+            // Inserting the value and type to graph
             var point = new DataPoint(chart.Series[0].Points.Count + 1, value);
             point.AxisLabel = type;
-            //point.Label = type; // Set the label to display the name on the bar
-            //point.LabelForeColor = Color.White; // Set the text color to white
 
+            // Defining color
             point.Color = Color.FromArgb(
                 64 + (chart.Series[0].Points.Count * 20) % 192,
                 64 + (chart.Series[0].Points.Count * 40) % 192,
                 64 + (chart.Series[0].Points.Count * 60) % 192
             );
-
+            
+            //Adding to Graph
             chart.Series[0].Points.Add(point);
-            numericUpDown_Input.Focus();
+
+            //After insert the values, clean the inputs and rturn focus to first input
             numericUpDown_Input.Value = 0;
             textBox_Type_Input.Text = String.Empty ;
+            numericUpDown_Input.Focus();
         }
 
         private void button_Pie_chart_Click(object sender, EventArgs e)
         {
-
+            // Checking if there are any negative values
             for (int i = 0; i < chart.Series[0].Points.Count; i++)
             {
                 if (chart.Series[0].Points[i].YValues[0] < 0)
